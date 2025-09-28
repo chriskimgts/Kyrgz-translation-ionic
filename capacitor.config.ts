710546babef42212e7ac5ddad8264a2c83560d04
chris kim <chriskim18@gmail.com>
2025-09-28 19:51:23 +0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kyrgz.translation',
+  appName: 'Kyrgz-translation-ionic',
+  webDir: 'www'
+};
+
+export default config;
